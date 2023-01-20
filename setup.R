9b352f33207b59f5b545c8cc7add02eddecf2a36
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2023-01-20 04:04:44 -0500

    install tidyverse rather than individual packages
    and add some comments to installation code

--- setup.R
+++ setup.R
@@ -1,9 +1,17 @@
 #!/usr/bin/env RScript
 
+# A function that installs packages
 installPackage <- function(pkg) {
 	install.packages(pkg, repos='http://cran.us.r-project.org', verbose = TRUE)
 }
 
-libs <- c("ggplot2", "readr", "extrafont", "Cairo", "devtools", "dplyr", "gridExtra", "reshape2")
+# A list of packages to install, including tidyverse
+# (this includes ggplot2, dplyr, and several others...)
+# https://www.tidyverse.org/packages/
+libs <- c("tidyverse", "extrafont", "Cairo", "devtools","gridExtra")
+
+# Apply the function above to the list of packages
 lapply(libs, installPackage)
+
+# Print successful completion message
 sprintf("Successfully loaded: %s", libs)
